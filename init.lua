fc8220ecf44f8d35fa9b4b1812e090dfd68e314d
Hubert <dtecnolife@proton.me>
2025-09-29 15:24:32 -0500

    change formatting

--- init.lua
+++ init.lua
@@ -38,7 +38,7 @@ vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('i', '<Esc>', '<nop>')
 vim.opt.number = true
 
--- Telescope
+-- telescope config
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
